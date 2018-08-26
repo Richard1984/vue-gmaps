45b7d3a645a8613c0ec3ced3f828d62ccaf9dd5e
Riccardo Sangiorgio <richard.sangiorgio@gmail.com>
2018-08-27 00:48:03 +0200

    Initial commit.

---
+++ src/components/infoWindow/infoWindow.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    <div ref="flyaway"> <!-- so named because it will fly away to another component -->
+      <slot>
+      </slot>
+    </div>
+  </div>
+</template>
+
+<script>
+export default (x => x.default || x )(require('./infoWindowImpl.js'))
+</script>
